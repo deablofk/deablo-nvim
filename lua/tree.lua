03c9bee3d7ec5893b4d8124a245734ab90cd46da
deablofk <deablofk@gmail.com>
2024-02-06 08:46:17 -0400

    change folder icon and name colors in neovim tree (explorer)

--- lua/tree.lua
+++ lua/tree.lua
@@ -2,7 +2,35 @@ require 'nvim-tree'.setup {
 	update_focused_file = {
 		enable = true,
 	},
+	renderer = {
+		icons = {
+			show = {
+				file = false,
+				folder = true
+			},
+			web_devicons = {
+				file = {
+					enable = true,
+					color = false,
+				},
+				folder = {
+					enable = false,
+					color = false,
+				},
+			},
+		}
+	}
 }
+
+local highlights = {
+	NvimTreeFolderName = { fg = '#ffdd33' },
+	NvimTreeOpenedFolderName = {fg = '#ffdd33'  },
+	NvimTreeFolderIcon = { fg = '#cc8c3c' },
+}
+
+for group, value in pairs(highlights) do
+	vim.api.nvim_set_hl(0, group, value)
+end
 
 vim.api.nvim_create_autocmd({ 'VimEnter' }, {
 	callback = function()
