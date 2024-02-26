2c4cf32f58938ab8af07ddce82e2df607fc7c3d6
deablofk <deablofk@gmail.com>
2024-02-26 05:49:51 -0400

    change tab from 4 to 2 spaces

--- lua/options.lua
+++ lua/options.lua
@@ -4,9 +4,9 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.nu = true
 vim.opt.rnu = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.termguicolors = true
